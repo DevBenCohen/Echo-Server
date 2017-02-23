2e585b3e7f2187c5e713c00730ce3cc30d9b7344
Ben Cohen <ben.cohenvi@gmail.com>
2017-02-23 20:49:14 +0200

    Finsished Tkinter
    Tkinter Finished got colors/ not crushing/ prints output and inputs

--- EchoClient1.py
+++ EchoClient1.py
@@ -1,4 +1,17 @@
 from socket import *
+from Tkinter import *
+import tkFont
+
+def close_tkwindow(root):
+    root.destroy()
+
+root = Tk()
+fnt = tkFont.Font(family="Fira Code", size=16)
+root.title("Inputs and Outputs")
+root.geometry("500x350")
+root.resizable(width=FALSE, height=FALSE)
+listbox = Listbox(root, font=fnt)
+listbox.pack(expand="YES", fill = "both")
 
 f = open("C:\\Users\user\Desktop\Details.txt", "r")
 file = f.read()
@@ -14,13 +27,21 @@ ADDR = (HOST, PORT)
 
 tcpCliSock = socket(AF_INET, SOCK_STREAM)
 tcpCliSock.connect(ADDR)
+try:
+    while 1:
+        data = raw_input(">")
+        listbox.insert(END,">"+data)
+        listbox.itemconfig(END, bg="#eeeeee")
+        if not data: break
+        tcpCliSock.send(data)
+        data = tcpCliSock.recv(BUFSIZ)d
+        if not data: break
+        if data == "exit": break
+        listbox.insert(END,data)
+        listbox.itemconfig(END, bg ="#dddddd")
 
-while 1:
-    data = raw_input(">")
-    if not data: break
-    tcpCliSock.send(data)
-    data = tcpCliSock.recv(BUFSIZ)
-    if not data: break
-    if data == "exit": break
-    print data
-tcpCliSock.close()
+    root.close_tkwindow()
+    tcpCliSock.close()
+    root.mainloop()
+except:
+    print "Finished"
